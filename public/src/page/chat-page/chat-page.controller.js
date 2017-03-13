36334c6d69d26135e1674cc9e9b44c14e21c909b
yuejiangming <yuejiangmingdx@gmail.com>
2017-03-14 04:46:42 +0900

    基本聊天功能调通

--- public/src/page/chat-page/chat-page.controller.js
+++ public/src/page/chat-page/chat-page.controller.js
@@ -1,7 +1,75 @@
 export default class ChatPageController{
-    constructor() {
-        var socket = new WebSocket();
+    constructor($scope) {
+        angular.extend(this, {$scope});
+
+        var self = this;
+
+        if(!getUserName()) {
+            return;
+        }
+
+        this.socket = new WebSocket('ws://127.0.0.1:8282');
+
+        this.socket.onopen = onopen;
+        this.socket.onclose = onclose;
+        this.socket.onerror = onerror;
+        this.socket.onmessage = onmessage;
+
+        this.chatContent = [];
+
+        var contentContainer = document.getElementsByClassName('content-container')[0];
+        
+        function onopen() {
+            var json = JSON.stringify({
+                type: 'connect',
+                user_name: self.userName
+            });
+
+            self.socket.send(json);
+        }
+
+        function onmessage(evt) {
+            var message = JSON.parse(evt.data);
+
+            switch(message.type) {
+                case 'message':
+                    var obj = {
+                        name: message.name,
+                        date: message.time,
+                        text: message.content
+                    }
+                    self.chatContent.push(obj);
+                    self.$scope.$apply();
+
+                    contentContainer.scrollTop = contentContainer.scrollHeight - contentContainer.clientHeight;
+                    break;
+            }
+        }
+
+        function getUserName() {
+            var name = prompt('请输入您的名字');
+            if (name != null && name != '') {
+                self.userName = name;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    submitWord() {
+        if (this.userWord == null || this.userWord == '') {
+            return;
+        }
+
+        var json = JSON.stringify({
+            type: "sent-to-all",
+            content: this.userWord
+        });
+        this.socket.send(json);
+
+        this.userWord = '';
     }
 }
 
-ChatPageController.$inject = [];
+ChatPageController.$inject = ['$scope'];
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('/build/main');
 });
+
+Route::get('{path?}', function() {
+    return view('/build/main');
+})->where('path', '.+');
